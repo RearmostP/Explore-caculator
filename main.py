2c40df5026f3e0ed71396f6bc25e18d96810c4ef
RearmostP <d0508422623@gmail.com>
2026-02-06 13:33:17 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from ui.main_ui import *
+
+class ExpenseApp(App):
+    def build(self):
+        return Root()
+
+if __name__ == "__main__":
+    ExpenseApp().run()
---
+++ ui/main_ui.py
@@ -0,0 +1,26 @@
+
+def save_expense(self):
+    item = self.root.ids.item.text
+    amount = self.root.ids.amount.text
+    
+    if not item or not amount:
+        return
+    
+    self.logic.add_item(
+        expense_name="כללי",
+        list_name="יומי",
+        item=item,
+        amount=amount
+    )
+    
+    self.root.ids.item.text = ""
+    self.root.ids.amount.text = ""
+
+
+def calc_month(self):
+    today = date.today()
+    total = self.logic.total_for_month(
+        str(today.year),
+        today.month
+    )
+    self.root.ids.total_label.text = f"סך החודש: ₪ {total}"
